2dbec9f37581334c59338e0c495ab8fb801f8a36
Carlos Bilbao <Zildj1an@users.noreply.github.com>
2019-05-01 18:44:12 +0200

    Significance function

---
+++ significance/significance.R
@@ -0,0 +1,15 @@
+# change the directory
+getwd()
+table_1 = read.table('bagging_column',header=FALSE) 
+table_2 = read.table("svc_column", header=FALSE)
+clf1 = table_1[[1]] 
+clf2 = table_2[[1]]
+ 
+if(length(clf1) > length(clf2)) {
+      clf1 <- head(clf1,n = length(clf2))
+}
+if(length(clf1) < length(clf2)){
+      clf2 <- head(clf2,n = 0:length(clf1))
+}
+
+mcnemar.test(clf1,clf2)
