601aacc4ffce07e2e8b92e196b9bb2475bde8478
Carlos Bilbao <Zildj1an@users.noreply.github.com>
2019-05-01 18:19:08 +0200

    Create significance.py

---
+++ significance.py
@@ -0,0 +1,10 @@
+# SIGNIFICANCE TEST (For R)
+def significance(file_name, y_pred,y_test):
+	f1 = open(file_name, 'w+')
+
+	for i in range(0,len(y_test)): 
+		if y_pred[i] == y_test[i]: 
+			f1.write("1") 
+		else: 
+			f1.write("0")
+	f1.close
